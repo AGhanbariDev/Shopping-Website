87ef2973c47747edd5b99aacf6822b531efbea21
AydinGh <aydin.ghanbari@yahoo.com>
2024-05-09 18:12:55 -0400

    Update main.js
    Some more additions to make the visuals of the login/register more functional.

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+// setFormMessage(loginForm, "success", "You're logged in!");
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
@@ -12,5 +22,13 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createAccountForm.classList.add("form--hidden");
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // Perform your AJAX/Fetch login
+
+        setFormMessage(loginForm, "error", "Invalid username or password");
     });
 });
